7db03900f7796eefaa43c8cbe9a35b5eba77ba94
walkingway <justinwayy@gmail.com>
2014-10-19 23:19:10 +0800

    first commit

--- Fortuna/AppDelegate.swift
+++ Fortuna/AppDelegate.swift
@@ -11,36 +11,68 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
+  var positiveQuotes: [String]! = []
+  var negativeQuotes: [String]! = []
 
+  func application(application: UIApplication, didFinishLaunchingWithOptions
+    launchOptions: [NSObject: AnyObject]?) -> Bool {
+      // Override point for customization after application launch.
+      let positiveQuotesPath = NSBundle.mainBundle().pathForResource("positiveQuotes", ofType: "json")
+      let negativeQuotesPath = NSBundle.mainBundle().pathForResource("negativeQuotes", ofType:"json")
+    
+      // TODO: load quotations from JSON files
+      
+      for string in loadJSON(positiveQuotesPath!) as [String]! {
+        positiveQuotes.append(string)
+      }
+      
+      for string in loadJSON(negativeQuotesPath!) as [String]! {
+        negativeQuotes.append(string)
+      }
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+      // Assertions to make sure that the quotations are loaded.
+      assert(positiveQuotes.count > 0, "should load positive quotes")
+      assert(negativeQuotes.count > 0, "should load negative quotes")
+      
+      return true
+  }
+  
+  func loadJSON(path: String) -> AnyObject? {
+    // Load data from path
+    let data = NSData(contentsOfFile: path)
+    assert(data != nil, "Failed to read data from: \(path)")
+    
+    // Parse JSON data
+    var err: NSError?
+    let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.allZeros, error: &err)
+    assert(err == nil, "Error parsing jsono: \(err)")
+    
+    return json
+  }
+  
+  func applicationWillResignActive(application: UIApplication) {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+  }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
+  func applicationDidEnterBackground(application: UIApplication) {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  }
 
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+  func applicationWillEnterForeground(application: UIApplication) {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+  }
 
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
+  func applicationDidBecomeActive(application: UIApplication) {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+  }
 
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
+  func applicationWillTerminate(application: UIApplication) {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  }
 
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+  
 }
 
--- Fortuna/ViewController.swift
+++ Fortuna/ViewController.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var quotationTextView: UITextView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func awakeFromNib() {
+    println("awakeFromNib quotationTextView: \(quotationTextView)")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    println("Hacking in Swift, since iOS" + "8")
+    
+    println("controller: \(self)")
+    println("view: \(self.view)")
+    println("view's frame: \(self.view.frame)")
+    println("viewDidLoad quotationTextView: \(quotationTextView)")
+    displayRandomQuote()
+    
+    quotationTextView.editable = false
+    quotationTextView.selectable = false
+  }
+  
+  func displayRandomQuote() {
+    let delegate = UIApplication.sharedApplication().delegate as AppDelegate
+    let arrayOfPositiveAndNegative = [delegate.positiveQuotes, delegate.negativeQuotes]
+    let quotes:[String] = arrayOfPositiveAndNegative[Int(arc4random_uniform(2))]
+    
+    let count = UInt32(quotes.count)
+    let quote: String = quotes[Int(arc4random_uniform(count))]
+    
+    quotationTextView.text = quote
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  @IBAction func rootViewTapped(sender: UITapGestureRecognizer) {
+    displayRandomQuote()
+  }
 
 
 }
